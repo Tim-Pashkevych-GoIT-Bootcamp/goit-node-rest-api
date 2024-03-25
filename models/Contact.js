e4d37a6d05d0e6898404d352786c4b484893a715
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-03-25 18:27:18 +0200

    Validation schema and Modal schema added

---
+++ models/Contact.js
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose";
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
