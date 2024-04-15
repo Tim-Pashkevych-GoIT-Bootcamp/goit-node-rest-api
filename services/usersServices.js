f5b26ee2e354ec8d60ab8d4563b35f8b7d45c9f0
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-04-15 13:30:51 +0300

    Generate default avatar for users using gravatar package

--- services/usersServices.js
+++ services/usersServices.js
@@ -1,11 +1,19 @@
 import bcrypt from "bcrypt";
+import gravatar from "gravatar";
+
 import { User } from "../models/User.js";
 
 const find = (filter) => User.findOne(filter);
 
 const create = async (data) => {
   const hashPassword = await bcrypt.hash(data.password, 10);
-  const user = await User.create({ ...data, password: hashPassword });
+  const avatarURL = gravatar.url(data.email);
+
+  const user = await User.create({
+    ...data,
+    password: hashPassword,
+    avatarURL,
+  });
 
   return user;
 };
