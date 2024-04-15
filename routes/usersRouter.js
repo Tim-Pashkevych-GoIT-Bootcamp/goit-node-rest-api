01e6125f92f328487fa6039c3614e39fb1cdd658
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-04-15 11:51:48 +0300

    Fixed bug in Joi schema

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import validateBody from "../decorators/validateBody.js";
-import { userSignupSchema } from "../schemas/usersSchemas.js";
+import { userSignupLoginSchema } from "../schemas/usersSchemas.js";
 import usersControllers from "../controllers/usersControllers.js";
 
 const usersRouter = express.Router();
 
 usersRouter.post(
   "/register",
-  validateBody(userSignupSchema),
+  validateBody(userSignupLoginSchema),
   usersControllers.register
 );
 
--- schemas/usersSchemas.js
+++ schemas/usersSchemas.js
@@ -1,8 +1,7 @@
 import Joi from "joi";
 import { emailRegexp } from "../constants/user-constants.js";
 
-export const userSignupSchema = Joi.object({
-  password: Joi.string().required(),
+export const userSignupLoginSchema = Joi.object({
   email: Joi.string().pattern(emailRegexp).required(),
   password: Joi.string().min(6).required(),
 });
